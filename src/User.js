e7082c59da3f61094fdc75275a93150fb74e63a1
phillipsalangsang <phillip_salangsang@hotmail.com>
2023-02-13 11:56:38 +0000

    Test updates still failing a few

--- src/User.js
+++ src/User.js
@@ -1,24 +1,3 @@
-// class User {
-//   constructor(username, password, age) {
-//     this.username = username;
-//     this.password = password;
-//     this.age = age;
-//     this.loggedIn = false;
-//   }
-
-//   login(password) {
-//     if (password === this.password) {
-//       this.loggedIn = true;
-//     } else {
-//       throw new Error("Incorrect password");
-//     }
-//   }
-
-//   logout() {
-//     this.loggedIn = false;
-//   }
-// }
-
 class User {
   constructor(username, password, age) {
     this.username = username;
