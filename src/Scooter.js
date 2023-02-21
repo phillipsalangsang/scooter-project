7cfd70e9352749a1379252f7f47d0f7c65cc73e9
phillipsalangsang <phillip_salangsang@hotmail.com>
2023-02-21 13:05:30 +0000

    updates

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,45 +1,43 @@
-class Scooter {
-  constructor(station) {
+class Scooter{
+    static nextSerial = 1;
+    constructor(station) {
       this.station = station;
-      this.user = null;
-      this.serial = Scooter.nextSerial++;
       this.charge = 100;
       this.isBroken = false;
-  }
-
-  rent() {
-      if (this.charge > 20 && !this.isBroken) {
-          this.station = null;
-          this.user = user;
-          return this;
-      } else if (this.charge <= 20) {
-          throw new Error("Scooter needs to charge");
+      this.user = null;
+      this.serial = Scooter.nextSerial;
+      Scooter.nextSerial++;
+    } 
+    rent() {
+      if((this.charge > 20) && !(this.isBroken)) {
+        return true;
+      } else if (this.charge > 20) {
+        throw new Error("scooter needs repair")
       } else {
-          throw new Error("Scooter needs repair");
+        throw new Error("scooter needs to charge")
       }
-  }
-
-  dock(station) {
+    }
+    dock(station) {
       this.station = station;
       this.user = null;
+    }
+    async recharge() {
+      console.log("Starting charging...");
+      while(this.charge < 100) {
+        await new Promise(resolve => setTimeout(resolve, 20));
+        this.charge++;
+        if(this.charge % 5 === 0) { 
+          console.log("Charge at: " + this.charge + "%")
+        }
+      }
+      console.log("Finished charging")
+    }
+    async requestRepair() {
+      await new Promise(resolve => setTimeout(resolve, 5000));
+      this.isBroken = false;
+      console.log("repair completed");
+    }
   }
-
-  async recharge() {
-      console.log('Starting charge');
-      await new Promise(resolve => setTimeout(resolve, 2000));
-      this.charge = 100;
-      console.log('Charge complete');
-  }
-
-  requestRepair() {
-      setInterval(() => {
-          this.isBroken = false;
-          console.log('Repair completed');
-      }, 5000);
-  }
-}
-
-Scooter.nextSerial = 1;
 
 
 module.exports = Scooter
